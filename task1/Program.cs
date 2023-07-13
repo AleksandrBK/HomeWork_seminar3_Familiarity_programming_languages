e83df76c3a5395552fe4fd6fb54b1fd12aa187a4
AleksandrBK <lebedevavlv@gmail.com>
2023-07-13 23:02:27 +0400

    Task 1.1

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,26 +4,73 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.Write("Введите пятизначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите пятизначное число: ");
+int number ;
 
-// if (number % 1 == number % 10000 || number % 100 == number % 1000)
-// {
-//     Console.WriteLine($"Число {number} является палиндромом");
-// }
-// else
-// {
-//     Console.WriteLine($"Число {number} не является палиндромом");
-// }
+while (true)
+{
+    Console.Write("Введите пятизначное число: ");
+    string input = Console.ReadLine();
 
-int number1 = number % 1;
-int number2 = number % 10;
-int number3 = (number % 100) / 10;
-int number4 = number % 1000;
-int number5 = number % 10000;
+    if (string.IsNullOrEmpty(input))
+    {
+        Console.WriteLine("Вы не ввели число, пожалуйста повторите попытку");
+        continue;
+    }
 
-Console.WriteLine($" {number} % 1 = {number1}");
-Console.WriteLine($" {number} % 10 = {number2}");
-Console.WriteLine($" {number} % 100 = {number3}");
-Console.WriteLine($" {number} % 1000 = {number4}");
-Console.WriteLine($" {number} % 10000 = {number5}");
+    if (!int.TryParse(input, out number))
+    {
+        Console.WriteLine("Вы ввели некорректное значение, пожалуйста повторите попытку");
+        continue;
+    }
+
+    if (number < 10000 || number >= 100000)
+    {
+        Console.WriteLine($"Вы ввели не пятизначное число >{number}<, пожалуйста повторите попытку");
+        continue;
+    }
+
+    
+
+    break;
+}
+
+static int ReverseNumber(int number)
+    {
+        int reversedNumber = 0;
+
+        while (number != 0)
+        {
+            int remainder = number % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            number /= 10;
+        }
+
+        return reversedNumber;
+    }
+
+    int reversedNumber = ReverseNumber(number);
+
+    if (number == reversedNumber)
+    {
+        Console.WriteLine($"Число {number} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} не является палиндромом");
+    }
+
+
+
+
+// int number1 = number % 1;
+// int number2 = number % 10;
+// int number3 = (number % 100) / 10;
+// int number4 = number % 1000;
+// int number5 = number % 10000;
+
+// Console.WriteLine($" {number} % 1 = {number1}");
+// Console.WriteLine($" {number} % 10 = {number2}");
+// Console.WriteLine($" {number} % 100 = {number3}");
+// Console.WriteLine($" {number} % 1000 = {number4}");
+// Console.WriteLine($" {number} % 10000 = {number5}");
