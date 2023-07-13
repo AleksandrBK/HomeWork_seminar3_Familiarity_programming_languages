3bd44b41be7a8e0bc58df3532bea8a79c8fb0fb0
AleksandrBK <lebedevavlv@gmail.com>
2023-07-14 00:01:22 +0400

    Task 3

--- task3/Program.cs
+++ task3/Program.cs
@@ -3,3 +3,20 @@
 // и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Таблица кубов чисел от 1 до " + N + ":");
+
+        for (int i = 1; i <= N; i++)
+        {
+            int cube = i * i * i;
+            Console.WriteLine(cube);
+        }
+    }
+}
